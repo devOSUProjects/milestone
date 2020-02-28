72ca36c66bb22fbce4e916d7c9832cc4aea47252
William Ward <imtheward@gmail.com>
2020-02-27 16:07:35 -0800

    Currently have 4 working functions for String
    Still want to improve on strcat as right now it's taking in lists of characters instead of just strings.

--- While.hs
+++ While.hs
@@ -110,3 +110,28 @@ strcmp [] [] = True
 strcmp a [] = False
 strcmp [] a = False
 strcmp (x:xs) (s:ss) = if ( x == s ) then strcmp xs ss else False
+
+--when given two strings it will concatenate them
+strcat :: [[a]] -> [[a]] -> [[a]]
+strcat [] _ = []
+strcat _ [] = []
+strcat (x:xs) (y:ys) = (x ++ y) : strcat xs ys
+
+--Returns the number of words in a word list (String)
+wordcount :: String -> Int
+wordcount = length . words
+
+--Checks if a given string is contained within the second string.
+--The string must be contained exactly as typed in the 2nd string or it will return false.
+strhas :: String -> String -> Bool
+strhas (_:_) [] = False
+strhas xs ys
+   | prefix xs ys = True
+   | strhas xs (tail ys) = True
+   | otherwise = False
+   
+prefix :: String -> String -> Bool
+prefix [] _ = True
+prefix (_:_) [] = False
+prefix (x:xs) (y:ys) = (x == y) && prefix xs ys
+
